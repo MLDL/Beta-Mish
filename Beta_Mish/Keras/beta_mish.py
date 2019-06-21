0afd6ec69f46d58bba561f7aef25811cf0fa3bad
Diganta Misra <mishradiganta91@gmail.com>
2019-06-21 10:34:36 +0530

    Added Keras Implementation

---
+++ Beta_Mish/Keras/beta_mish.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Layers that act as activation functions.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras.engine.base_layer import Layer
+from keras import backend as K
+
+class beta_mish(Layer):
+    """Beta Mish Activation Function.
+    It follows:
+    `f(x) = x * tanh(ln((1 + exp(x))^\\beta))`.
+    # Input shape
+        Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+    # Output shape
+        Same shape as the input.
+    # Arguments
+        beta: A constant or a trainable parameter (default = 1.5)
+    # References
+        - [β-Mish: An uni-parametric adaptive activation function derived from Mish.](
+           https://github.com/digantamisra98/Beta-Mish)
+    """
+
+    def __init__(self, beta=1.5, **kwargs):
+        super(beta_mish, self).__init__(**kwargs)
+        self.supports_masking = True
+        self.beta = K.cast_to_floatx(beta)
+
+    def call(self, inputs):
+        return inputs*K.tanh(K.log(K.pow((1+K.exp(inputs)),self.beta)))
+
+    def get_config(self):
+        config = {'beta': float(self.beta)}
+        base_config = super(beta_mish, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
